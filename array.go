39783cce5f9fe5b62560fd22a598a8ad6c14b131
pumppee27 <cahyakw27@gmail.com>
2024-05-22 09:11:32 +0700

    edit array.go

--- array.go
+++ array.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fruits := []string{"Apel", "Nangka"}
+	fruits := [3]string{"Apel", "Nangka"}
 
 		for i, j :=range fruits{
 			fmt.Println("index", i, "Nama", j)
