69dfd33ecfd074ba7a3a442fabc30d5e8675071e
pumppee27 <cahyakw27@gmail.com>
2024-05-22 09:10:19 +0700

    create slice.go

--- array.go
+++ array.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fruits := [...]string{"Apel", "Nangka"}
+	fruits := []string{"Apel", "Nangka"}
 
 		for i, j :=range fruits{
 			fmt.Println("index", i, "Nama", j)
---
+++ slice.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	nama := []any{"Budi", 1}
+
+	fmt.Println(nama)
+}
